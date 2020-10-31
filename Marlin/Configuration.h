666aed0289a372f73e2405b0975b374ccc3ce805
Alexandre Viau <alexandre.viau.2@etsmtl.net>
2020-10-30 21:12:26 -0400

    ajustement setting extrudeur

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -738,7 +738,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 582 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 669 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -983,7 +983,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -52, 0, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -52, 0, 2.95 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
