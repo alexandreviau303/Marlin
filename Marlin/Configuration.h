9d091d07552144b140b2c8ab34c1a283282fcc60
Alexandre Viau <alexandre.viau.2@etsmtl.net>
2020-11-02 07:03:00 -0500

    update general

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -983,7 +983,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -52, 0, 2.95 }
+#define NOZZLE_TO_PROBE_OFFSET { -52, 0, 2.85 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1239,7 +1239,7 @@
  * Normally G28 leaves leveling disabled on completion. Enable
  * this option to have G28 restore the prior leveling state.
  */
-//#define RESTORE_LEVELING_AFTER_G28
+#define RESTORE_LEVELING_AFTER_G28
 
 /**
  * Enable detailed logging of G28, G29, M48, etc.
