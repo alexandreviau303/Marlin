291838d9c3c822ba58ac07363cde4b152e5249d4
Alexandre Viau <alexandre.viau@dyzedesign.com>
2021-02-21 15:54:22 -0500

    change parameter for new holder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -983,7 +983,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -38, 3, -2.85 }
+#define NOZZLE_TO_PROBE_OFFSET { -34, 5, -3.200 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1120,11 +1120,11 @@
 
 // The size of the print bed
 #define X_BED_SIZE 235  
-#define Y_BED_SIZE 220
+#define Y_BED_SIZE 230
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -4
-#define Y_MIN_POS -21
+#define X_MIN_POS -5
+#define Y_MIN_POS -12
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
